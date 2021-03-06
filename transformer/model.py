e176e783257993fcc4a4e5427f4dd65d896accde
Sebastien <sjeanpie@cs.brown.edu>
2021-03-06 11:32:15 -0500

    transformer stencil

---
+++ transformer/model.py
@@ -0,0 +1,11 @@
+import torch
+import torch.nn as nn
+
+
+class Transformer(nn.Module):
+    def __init__(self, *args, **kwargs):
+        super(Transformer, self).__init__()
+        pass
+
+    def forward(self, *input: Any, **kwargs: Any):
+        pass
