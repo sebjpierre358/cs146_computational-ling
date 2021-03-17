ac9271e5857b6af0ea706d71586b4f842ac19231
Sebastien <sjeanpie@cs.brown.edu>
2021-03-17 08:14:23 -0400

    gpt2 stencil

---
+++ gpt2/transformer.py
@@ -0,0 +1,14 @@
+import torch
+import torch.nn as nn
+
+class Transformer(nn.Module):
+    def __init__(self, *args):
+        super(Transformer, self).__init__()
+        '''
+        You can copy your transformer from the previous assignment. You can also create a new Transformer using torch.nn.transformer modules!
+        This transformer class will be trained exactly as the previous assignment.
+        '''
+        pass
+
+    def forward(self, input):
+        pass
